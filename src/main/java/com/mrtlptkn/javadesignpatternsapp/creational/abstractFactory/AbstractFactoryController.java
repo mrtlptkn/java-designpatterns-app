948fcf5f1a3cb591c4081667db193f257d3f9fc4
neominalbilisim <info@neominal.com>
2026-06-10 11:25:56 +0300

    Abstract Factory patterns

--- src/main/java/com/mrtlptkn/javadesignpatternsapp/creational/abstractFactory/AbstractFactoryController.java
+++ src/main/java/com/mrtlptkn/javadesignpatternsapp/creational/abstractFactory/AbstractFactoryController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/abstract-factory")
-public class AbstractFctoryController {
+public class AbstractFactoryController {
 
 
     @GetMapping("test")
